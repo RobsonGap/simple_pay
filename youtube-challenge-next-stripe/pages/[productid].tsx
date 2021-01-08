84353a5e3e867f6902bd2799e99caab20b975e79
Robsongap <robsonmie@hotmail.com>
2021-01-07 23:57:41 -0300

    listing products
    listing products create and exports use key

--- youtube-challenge-next-stripe/pages/[productid].tsx
+++ youtube-challenge-next-stripe/pages/[productid].tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import Stripe from 'stripe';
-import { GetStaticPaths } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 
 import stripeConfig from '../config/stripe'
  
 const Product: React.FC = ()  => {
-  return <h1>Simple Stripe Store</h1>
+  return <h1>Product</h1>
 };
 
-const getStaticPaths: GetStaticPaths = async () => {
-  const stripe = new Stripe(stripeConfig.secretKey {
-    apiVersion: '2020-08-27'
-  })
+export const getStaticPaths: GetStaticPaths = async () => {
+  const stripe = new Stripe(stripeConfig.secretKey, {
+    apiVersion: '2020-08-27',
+  });
 
- 
+ const skus = await stripe.skus.list();
+
+ console.log(skus.data);
 
   return {
     paths: [],
-    fallback: false
-  }
-}
+    fallback: false,
+  };
+};
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  return  {
+    props: {},
+  };
+};
 
 export default Product;
  
