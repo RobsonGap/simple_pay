4b094873334536657728595279263bc07b259156
Robsongap <robsonmie@hotmail.com>
2021-01-10 16:24:20 -0300

    Creating the  Visit page
    creating the visit page by creating small components

--- youtube-challenge-next-stripe/pages/index.tsx
+++ youtube-challenge-next-stripe/pages/index.tsx
@@ -1,8 +1,54 @@
-import Head from 'next/head'
+import { GetStaticProps } from "next";
+import Stripe from "stripe";
+import Link from 'next/link';
 
+import stripeConfig from '../config/stripe';
 
-export default function Home() {
-  return (
-    <div>Rocketseat</div>
-  )
+interface Props {
+  skus: Stripe.Sku[];
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const stripe = new Stripe(stripeConfig.secretKey, {
+    apiVersion: '2020-08-27',
+  });
+
+  const skus = await stripe.skus.list();
+
+  return {
+    props: {
+      skus: skus.data,
+    }
+  };
+};
+
+const HomePage: React.FC<Props> = ({ skus }) => {
+  return ( 
+    <>
+     <h1>Simple Stripe Store</h1>;
+
+    {skus.map(sku => (
+      <div key={sku.id}>
+        <h1>{sku.attributes.name}</h1>
+
+        {sku.image && (
+          <img 
+            src={sku.image}
+            style={{
+              width: '100px',
+            }}
+            />
+        )}
+
+        <h2>
+          {Number(sku.price / 100).toFixed(2)} {sku.currency.toUpperCase()}
+        </h2>
+
+        <Link href="/">Visit Page</Link>
+        <hr/>
+      </div>
+    ))}
+  </>
+  );
+}
+export default HomePage;
